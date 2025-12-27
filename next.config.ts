51f410140342ba1162f9c5483d8565ea42124b2f
congthinhcttt <thinhthcshd@gmail.com>
2025-12-27 21:39:53 +0700

    Fix config for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,22 @@
 import type { NextConfig } from "next";
 
+const repo = "My_Portfolio"; // ✅ đúng tên repo trong URL
+
 const nextConfig: NextConfig = {
+  output: "export",
+
+  // GitHub Pages không có Image Optimization server
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "picsum.photos" },
     ],
   },
+
+  // chạy dưới subpath /My_Portfolio/
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
